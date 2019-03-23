c906310a07b655c23f6c4cab7ec691fe9253930b
John Sheahan <john.r.sheahan@gmail.com>
2019-03-23 16:33:04 +1100

    realize both interrupts are now on the same vector

--- weatherino.ino
+++ weatherino.ino
@@ -1,19 +1,18 @@
 //    -*- Mode: c++     -*-
 // emacs automagically updates the timestamp field on save
-// my $ver =  'weatherino temp humidity pressure rainfall wind for moteino Time-stamp: "2018-11-20 19:41:06 john"';
+// my $ver =  'weatherino temp humidity pressure rainfall wind for moteino Time-stamp: "2019-03-23 16:31:32 john"';
 
 // $ grabserial -b 19200 -d /dev/ttyUSB1 | ts [%y%m%d%H%M%S]
 
 
 
-#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
-#include <SPI.h>      //included with Arduino IDE (www.arduino.cc)
-#include <LowPower.h> //get library from: https://github.com/lowpowerlab/lowpower
-                      //writeup here: http://www.rocketscream.com/blog/2011/07/04/lightweight-low-power-arduino-library/
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
-#include <Adafruit_SHT31.h>
+#include <RFM69.h>         //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <RFM69_ATC.h>     //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPIFlash.h>      //get it here: https://www.github.com/lowpowerlab/spiflash
+#include <SPI.h>           //included with Arduino IDE install (www.arduino.cc)
+#include <RFM69_OTA.h>     // allow OTA reprogramming
+#include <LowPower.h>      //get library from: https://github.com/lowpowerlab/lowpower
+                           //writeup here: http://www.rocketscream.com/blog/2011/07/04/lightweight-low-power-arduino-library/
 
 
 
@@ -27,7 +26,7 @@
 #define GATEWAYID     1  //node Id of the receiver we are sending data to
 #define NETWORKID     100  //the same on all nodes that talk to each other including this node and the gateway
 #define FREQUENCY     RF69_915MHZ //others: RF69_433MHZ, RF69_868MHZ (this must match the RFM69 freq you have on your Moteino)
-// #define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
+#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
 // #define USE_ENCRYP
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
 #define SENDLOOPS    80 //default:80 //if no message was sent for this many sleep loops/cycles, then force a send
@@ -35,10 +34,10 @@
 
 //*********************************************************************************************
 #define SLEEP_FASTEST SLEEP_15MS
-#define SLEEP_FAST SLEEP_250MS
-#define SLEEP_SEC SLEEP_1S
-#define SLEEP_LONG SLEEP_2S
-#define SLEEP_LONGER SLEEP_4S
+#define SLEEP_FAST    SLEEP_250MS
+#define SLEEP_SEC     SLEEP_1S
+#define SLEEP_LONG    SLEEP_2S
+#define SLEEP_LONGER  SLEEP_4S
 #define SLEEP_LONGEST SLEEP_8S
 
 // period_t sleepTime = SLEEP_LONGEST; //period_t is an enum type defined in the LowPower library (LowPower.h)
@@ -77,9 +76,10 @@ period_t sleepTime = SLEEP_2S;    //period_t is an enum type defined in the LowP
 // rain interrupt  7
 // wind interrupt  8
 // wind direction  A7
-#define WIND_DRN_CH 7
-#define RainInt 7
-#define WindInt 8
+#define WIND_DRN_ADC_CH 7
+#define BATT_ADC_CH 6
+#define RainInt 6
+#define WindInt 7
 //
 // timers used 
 
@@ -115,22 +115,17 @@ period_t sleepTime = SLEEP_2S;    //period_t is an enum type defined in the LowP
 
 
 #ifdef SERIAL_EN
-  #define SERIAL_BAUD   19200
-  #define DEBUG(input)   {Serial.print(input);}
-  #define DEBUGln(input) {Serial.println(input);}
-  #define SERIALFLUSH() {Serial.flush();}
+#define SERIAL_BAUD   19200
+#define DEBUG(input)   {Serial.print(input);}
+#define DEBUGln(input) {Serial.println(input);}
+#define SERIALFLUSH() {Serial.flush();}
 #else
-  #define DEBUG(input);
-  #define DEBUGln(input);
-  #define SERIALFLUSH();
+#define DEBUG(input);
+#define DEBUGln(input);
+#define SERIALFLUSH();
 #endif
 
-// #define BATT_MONITOR  A7  // Sense VBAT_COND signal (when powered externally should read ~3.25v/3.3v (1000-1023), when external power is cutoff it should start reading around 2.85v/3.3v * 1023 ~= 883 (ratio given by 10k+4.7K divider from VBAT_COND = 1.47 multiplier)
-// #define BATT_READ_LOOPS  SENDLOOPS  // read and report battery voltage every this many sleep cycles (ex 30cycles * 8sec sleep = 240sec/4min). For 450 cycles you would get ~1 hour intervals between readings
-// #define BATT_FORMULA(reading) reading * 0.00322 * 1.475  // >>> fine tune this parameter to match your voltage when fully charged
-// #define BATT_LOW      3.55
 
-byte sendLen;
 byte sendLoops=0;
 //byte distReadLoops=0;
 //byte battReadLoops=0;
@@ -141,40 +136,41 @@ byte sendLoops=0;
 char buff[BUFLEN]; //this is just an empty string used as a buffer to place the payload for the radio
 char buff2[10];    // for float conversion
 
-// char* BATstr="BAT:5.00v"; //longest battery voltage reading message = 9chars
-// char* DISTstr="99999.99cm"; //longest distance reading message = 5chars
-// float readDistance(byte samples=1);  //take 1 samples by default
+
 RFM69 radio;
+SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 
-int outerloop_times;
+int windloop_times;
+int batteryloop_times;
 
+// due to a lightning strike, separating weatherino into 2 parts. This part is physically high
+// mounted near the rain and wind sensors. Want short wires due to high field strength.
+// Temperature sensors are 1.2m off the ground. If I connect to them too I get long wires.
 
-Adafruit_BMP280 bme; // I2C
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
+#define RAIN
+#define WIND
+#define BATTERY
 
-unsigned int rain_raw;
+// about 4 times per day is 6 hrs or 360 minutes 
+// with 8 minute inner loop update
+#define BATTERYLOOP_TIMES 45
+
+unsigned int  rain_raw;
 unsigned long wind_raw;
 
 long unsigned current_msec;
 long unsigned last_gust_msec;
-unsigned int gust_period;
-unsigned int used_gust_period;
-float biggest_gust;
-float this_gust;
-unsigned int this_gust_count;
+unsigned int  gust_period;
+unsigned int  used_gust_period;
+float         biggest_gust;
+float         this_gust;
+unsigned int  this_gust_count;
 long unsigned this_wind_count;
 long unsigned last_wind_count;
   
 
 
-// #define SERIAL_EN
-
-
 void setup() {
-  // noInterrupts();
-  // CLKPR = _BV(CLKPCE);  // enable change of the clock prescaler
-  // CLKPR = _BV(CLKPS0);  // divide frequency by 2
-  // interrupts();
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
 #ifdef IS_RFM69HW_HCW
   radio.setHighPower(); //must include this only for RFM69HW/HCW!
@@ -190,66 +186,47 @@ void setup() {
   Serial.println(F("BMP280 test"));
 #endif
   
-  sprintf(buff, "%02x weatherino", NODEID );  
-  sendLen = strlen(buff);
-  //  radio.sendWithRetry(GATEWAYID, buff, sendLen);
-  radio.sendWithRetry(GATEWAYID, buff, sendLen);
+  sprintf(buff, "%02x weatherino 20190317", NODEID );  
+  radio.sendWithRetry(GATEWAYID, buff, strlen(buff));
+
+  flash.initialize();
   
-  outerloop_times = 0;
-  
-  if (!bme.begin())
-    {  
-      sprintf(buff, "%02x Could not find a valid BMP280 sensor", NODEID);  
-      sendLen = strlen(buff);
-      radio.sendWithRetry(GATEWAYID, buff, sendLen);
-    }
-  
-  if (!sht31.begin(0x44))
-    {   // Set to 0x45 for alternate i2c addr
-      sprintf(buff, "%02x Could not find a valid SHT31 sensor", NODEID );  
-      sendLen = strlen(buff);
-      radio.sendWithRetry(GATEWAYID, buff, sendLen);
-    }
+  windloop_times = 0;
+  batteryloop_times = 0;
     
-    
-  //  radio.sleep();
-  
-  //  pinMode(TRIG, OUTPUT);
-  //  pinMode(FLOATSW, INPUT);
-  //  pinMode(SENSOR_EN, OUTPUT);
-    //  digitalWrite(SENSOR_EN, LOW);
-  
+  radio.sleep();
+
+#ifdef RAIN
   pinMode(RainInt, INPUT);
-  pinMode(WindInt, INPUT);
-  
   // enable interrupt for pins
   pciSetup(RainInt);
+  rain_raw = 0;
+#endif
+  
+#ifdef WIND
+  pinMode(WindInt, INPUT);
+  // enable interrupt for pins
   pciSetup(WindInt);
 
-  rain_raw = 0;
   wind_raw = 0;
-
   last_gust_msec = millis();
   biggest_gust = 0;;
   last_wind_count = 0;
+#endif
+
 }
 
 /************************** MAIN ***************/
 
-
-// #define BMP_SCK 13
-// #define BMP_MISO 12
-// #define BMP_MOSI 11 
-// #define BMP_CS 10
-
-//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
   
 #define min_gust_period 8400
 
   
 void loop() {
   
+  int batt_adc;
+  int ref_v;
+  float batt_v;
 
   // gust update
   current_msec = millis();
@@ -273,58 +250,23 @@ void loop() {
 	}
     }
   
-  if (outerloop_times == 0) {
-  
-    //    Serial.print("Approx altitude = ");
-    //    Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
+  if (windloop_times == 0) {
 
-
-    // *************** bmp280 temp and pressure **************
     
-    // no %f in sprintf
-    dtostrf(bme.readTemperature(), 5, 2, buff2);
-
-    sprintf(buff, "%02x bmp280Temperature=%s°C", NODEID, buff2  );  
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-
-    // no %f in sprintf
-    dtostrf(bme.readPressure(), 8, 2, buff2);
-
-    snprintf(buff, BUFLEN, "%02x Pressure=%s Pa", NODEID, buff2   );  
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-
-    // *************** sht31 temp and humidity **************
-
-    float t = sht31.readTemperature();
-    float h = sht31.readHumidity();
-
-    dtostrf(t, 5, 2, buff2);
-    sprintf(buff, "%02x sht31Temperature=%s°C", NODEID, buff2  );  
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-        
-    dtostrf(h, 5, 2, buff2);
-    sprintf(buff, "%02x sht31Humidity=%s%%", NODEID, buff2  );  
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-
     // *************** rainfall  **************
     // rain count
-    float r = get_rain_count() / 5.0;
+    float r = get_rain_count() / 5.0;  // 0.2mm per blip on int pin
     dtostrf(r, 5, 1, buff2);
     
     sprintf(buff, "%02x rainfall=%smm", NODEID, buff2 );  
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
+    radio.sendWithRetry(GATEWAYID, buff, strlen(buff));
     
     
     // *************** wind direction **************
 
     int drn;
     unsigned int drn_temp;
-    drn = analogRead(WIND_DRN_CH);
+    drn = analogRead(WIND_DRN_ADC_CH);
     drn = drn + drn_cal
 ;
     if (drn > 1023)
@@ -337,48 +279,64 @@ void loop() {
     drn_temp = 36 * drn ;
     
     sprintf(buff, "%02x WindDrn=%s %u°", NODEID, buff2, drn_temp / 103);
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-
+    radio.sendWithRetry(GATEWAYID, buff, strlen(buff));
+    delay(50);
+    
     // **************** wind count *****************
     dtostrf(biggest_gust, 5, 1, buff2);
-   
     sprintf(buff, "%02x Wind gust=%s Kph dist=%lu", NODEID, buff2, get_wind_count());
-    sendLen = strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, sendLen);
-    // sprintf(buff, "%02x gust_period=%u gust_count=%u", NODEID, used_gust_period, this_gust_count);
-    // sendLen = strlen(buff);
-    // radio.sendWithRetry(GATEWAYID, buff, sendLen);
+    radio.sendWithRetry(GATEWAYID, buff, strlen(buff));
     biggest_gust=0;
-    
-//    sprintf(buff, "%02x millis=%lu ", NODEID, millis());
-//    sendLen = strlen(buff);
-//    radio.sendWithRetry(GATEWAYID, buff, sendLen);
 
+    windloop_times = SLEEP_MULTIPLIER;
+
+    if (batteryloop_times == 0)
+      {
+	delay(50);
+	batt_adc =  analogRead(BATT_ADC_CH);
+	ref_v =  readVref();
+
+	// shouldn't happen, but whatever.  Clearly gives an incorrect value.
+	if (ref_v == 0)
+	    ref_v = 1;
+
+	// restore the Vdd adc reference after futzing with it as part of readVref
+	analogReference(DEFAULT);
+      
+	// reading the reference as a fraction of Vdd allows me to calculate vdd
+	// (1) Vdd = 1023 * v_ref / ref_v;
+	// check:  vdd = 1023 * 1.1 / 337 = 3.34
+	
+	// then reading the scaled external voltage as a fraction of Vdd allows me to calculate ext voltage
+	// (2) batt_v = (1000000 + 360000) / 1000000 * Vdd * (batt_adc / 1023);
+	// check: batt_adc = 928, batt_v = 4.12
+	// expanding
+	// (3) batt_v = (1000000 + 360000) / 1000000 * 1023 * (v_ref /ref_v) * (batt_adc / 1023);
+	// extracting constants
+	// (4) batt_v = v_ref * (1000000 + 360000) / 1000000 )  * batt_adc / ref_v );
+	// check 1.496 * 929 / 337 = 4.12
+	// actual 4.07
+	// implies ref is really 4.12 / 4.07 = 1.085
+	
+	batt_v = 1.476 * batt_adc / ref_v; 
+	dtostrf(batt_v, 5, 2, buff2);
+	sprintf(buff, "%02x Batt=%sV", NODEID, buff2  );  
+	radio.sendWithRetry(GATEWAYID, buff, strlen(buff));
+	
+	batteryloop_times = BATTERYLOOP_TIMES;
+	
+      }
+    batteryloop_times--;
     
-    
-    radio.sleep();
-    //    delay(2000);
-    outerloop_times = SLEEP_MULTIPLIER;
   }
-  outerloop_times--;
+  if (radio.receiveDone())
+    CheckForWirelessHEX(radio, flash, true);
+  
+  
+  windloop_times--;
   delay(2000);
   // LowPower.powerDown(sleepTime, ADC_OFF, BOD_OFF); //put microcontroller to sleep to save battery life
 }
-
-
-
-//void Blink(byte pin)
-//{
-//  pinMode(pin, OUTPUT);
-//  digitalWrite(pin, HIGH);
-//  delay(2);
-//  digitalWrite(pin, LOW);
-//}
-//
-
-// rainfall
-
 
 
 
@@ -427,52 +385,65 @@ void dirn2str(uint16_t adcval, char *drnstr)
  
 void pciSetup(byte pin)
 {
+  cli();
   *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
   PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
   PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+  sei();
 }
 
 // Use one Routine to handle each group
 
-// wind int, which seems not to need a debounce
-ISR (PCINT0_vect) // handle pin change interrupt for D8 to D13 here
-{
-  static byte lastWindLvl = 0;
-  
-  // read the port, once.
-  byte WindLvl = digitalRead(WindInt);
-  
-  // negedge only
-  if ((lastWindLvl == HIGH) && (WindLvl == LOW))
-    {
-      wind_raw++;
-    }  
-  lastWindLvl = WindLvl;  // save for next time 
-  
-  digitalWrite(13,digitalRead(WindInt));
-}
+// oops, D8 is used by eerom. Wind is on D6 on this version of weatherino (rain + wind only)
 
+//ISR (PCINT0_vect) // handle pin change interrupt for D8 to D13 here
+//{
+//  static byte lastWindLvl = 0;
+//  
+//  // read the port, once.
+//  byte WindLvl = digitalRead(WindInt);
+//  
+//  // negedge only
+//  if ((lastWindLvl == HIGH) && (WindLvl == LOW))
+//    {
+//      wind_raw++;
+//    }  
+//  lastWindLvl = WindLvl;  // save for next time 
+//  
+//  digitalWrite(13,digitalRead(WindInt));
+//}
+//
 
 // RainInt - debounced
+// wind int, which seems not to need a debounce. Its a 1ms pulse.
+// at least on both I want to use negedge only
 ISR (PCINT2_vect) // handle pin change interrupt for D0 to D7 here
 {
-  static unsigned long last_interrupt_time = 0;
+  static unsigned long last_used_rain_interrupt_time = 0;
+  static byte last_wind_lvl = 1;
+  static byte last_rain_lvl = 1;
   unsigned long interrupt_time = millis();
-  byte lvl = digitalRead(RainInt);
-
-  // only use negedge
-    if (lvl == LOW)
-      {
-	// debounce
-	if ((interrupt_time - last_interrupt_time) > 100)
-	  { // apply 50ms debounce on rain transition
-	    
-	    // accumulate
-	    rain_raw++;
-	  }
-      }
-    last_interrupt_time = interrupt_time;
-    digitalWrite(13,lvl);
+  byte wind_lvl = digitalRead(WindInt);
+  byte rain_lvl = digitalRead(RainInt);
+  
+  // use only negedge for wind
+  if ((last_wind_lvl == HIGH) && (wind_lvl == LOW))
+    wind_raw ++;
+  
+  last_wind_lvl = wind_lvl;
+  
+  // only use negedge for rain, 
+  if ((last_rain_lvl == HIGH) && (rain_lvl == LOW))
+    // and debounce it at 100ms.
+      if (abs(interrupt_time - last_used_rain_interrupt_time) > 50)
+	// apply 50ms debounce on rain transition.
+	// the abs() is to cope with wrap around of millis. Doesn't happen often, but I'd lose a lot when it does finally wrap. 
+	{
+	  last_used_rain_interrupt_time = interrupt_time;
+	  rain_raw++;
+	}
+  last_rain_lvl = rain_lvl;
+  // digitalWrite(13,lvl);
 }
 
 unsigned long  get_wind_count (void)
@@ -493,6 +464,31 @@ unsigned int get_rain_count (void)
   return (rain_count);
 }
 
+
+int readVref() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  int result = (high<<8) | low;
+  
+  return result; // Vref as a fraction of vcc
+}
 
  
  
